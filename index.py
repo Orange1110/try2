b0817ccbc29c308dc176170d00207afa762d6277
Orange1110 <s1103014@o365st.pu.edu.tw>
2022-11-07 14:50:08 +0800

    version 0: html5

--- index.py
+++ index.py
@@ -66,10 +66,10 @@ def read():
         docs = collection_ref.get()
         for doc in docs:
             dict = doc.to_dict()
-            if keyword in dict["Course"] :
+            if keyword in dict["Course"]:
                 result += format(dict["Leacture"])+"老師開的"+format(dict["Course"])+"課程,每週"+format(dict["Time"])+"於"+format(dict["Room"])+"上課<br>"
            
-        if result == ""
+        if result == "":
             result = "抱歉，查無相關條件的選修課程"
 
         return result
